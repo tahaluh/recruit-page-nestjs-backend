e472ee5c8e08cc7f8588051cd0ef2e12afcac7d0
Thaua <thauanlucascpl@gmail.com>
2022-09-20 05:49:03 -0300

    cors update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
   .setTitle('API')
@@ -11,6 +12,7 @@ async function bootstrap() {
   .setVersion('1.0')
   .addTag('Client')
   .build();
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
